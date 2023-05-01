f989c7d0edf89c89122a3eb9dc9238b269f740a4
GERI12326 <114013384+GERI12326@users.noreply.github.com>
2023-05-01 21:00:49 +0200

    laravel

---
+++ laravel-to-do-api/storage/framework/views/b766007934291b3469c398395b0cbd72.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Germanium.Morina\Desktop\Projektet-CodersHub\CRUD-Project\laravel-to-do-api\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
